935e4094c94b9f7cd2a35424c3596b903d92e720
Ying Li <yil4013@med.cornell>
2023-09-01 14:19:38 -0500

    update format

--- R/bacteria.R
+++ R/bacteria.R
@@ -1,4 +1,4 @@
 #' Raw Taxon Count Table
 #'
-#' The data is a raw taxon count table consisting of 63 samples and 139 taxa.
+#' The data is a raw taxon count table consisting of 63 samples and 139 taxa
 "bacteria"
--- R/gvhd.R
+++ R/gvhd.R
@@ -1,4 +1,4 @@
 #' Survival Data
 #'
-#' The data is a survival object containing survival time and censoring/event information for 63 samples.
+#' The data is a survival object containing survival time and censoring/event information for 63 samples
 "gvhd"
